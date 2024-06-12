f20fac3aba9364d8547646e45f31a0f986dc15d6
growni <96620040+growni@users.noreply.github.com>
2024-06-13 01:43:36 +0300

    Dockerfile

--- src/main/java/com/ipsos/Runner.java
+++ src/main/java/com/ipsos/Runner.java
@@ -36,7 +36,7 @@ public class Runner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        createEntities();
+//        createEntities();
 //        operations();
     }
 
